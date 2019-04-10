ace26e0a03d29944157f1651fd7126eec359603a
Irakli12 <48749541+Irakli12@users.noreply.github.com>
2019-04-10 00:29:34 -0700

    vick_1
    lab 511 , lab 512

---
+++ wick1/lab511/lab511/tvitmfrinavi.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab511
+{
+    class tvitmfrinavi
+    {
+        private double bakisTevadoba;
+        private double mandzili1Litrit;
+        public int mgzavrebisRaodenoba;
+        public int gayiduliBiletebi;
+    }
+}
---
+++ wick1/lab512/lab512/student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab512
+{
+    class student
+    {
+        private string firstname;
+        private string name;
+        private int age;
+        public string universiti;
+        public int kursi;
+
+    }
+}
