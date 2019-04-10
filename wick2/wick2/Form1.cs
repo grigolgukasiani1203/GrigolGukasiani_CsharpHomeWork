b4bfdaaa6b31d0dc1a800680fb61b846813a1e29
Irakli12 <48749541+Irakli12@users.noreply.github.com>
2019-04-10 00:55:40 -0700

    wick
    lab 521

---
+++ wick2/wick2/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace wick2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[] nishnebi = { 100, 67, 87, 98, 60, 98, 67, 59, 78, 81 };
+            student studentobject = new student();
+            label1.Text = studentobject.sashualoAritmetikuli(nishnebi).ToString();
+        }
+    }
+}
---
+++ wick2/wick2/student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wick2
+{
+    class student
+    {
+        public double sashualoAritmetikuli(int[] nishnebi)
+        {
+            double sashualo = 0 , jami =0;
+            for (int i=0; i < nishnebi.Length; i++)
+            {
+                jami += nishnebi[i];
+            }
+            sashualo = jami / nishnebi.Length;
+            return sashualo;
+        }
+
+    }
+}
